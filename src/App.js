c112fd1e4e9404b1869a2ce929f774e0ccc0654c
Rafael León <r.leontorres1988@gmail.com>
2024-07-22 13:09:48 -0600

    Resuelto conflicto de fusión

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 import Navigation from './components/Navbar';
 import Home from './components/Home';
 import Skills from './components/Skills';
