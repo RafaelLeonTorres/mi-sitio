e2298da6e71e1a5fc7fa8247d6b267de1ddeb2d5
Rafael León <r.leontorres1988@gmail.com>
2024-07-22 20:18:51 -0600

    Ajustes

--- src/index.js
+++ src/index.js
@@ -1,17 +1,23 @@
 // src/index.js
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter, BrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';  // Importa Bootstrap CSS
 import './styles.css';
 import './i18n';
 
+// Determina el entorno de ejecución
+const isProduction = process.env.NODE_ENV === 'production';
+
+// Selecciona el Router basado en el entorno
+const Router = isProduction ? HashRouter : BrowserRouter;
+
 
 ReactDOM.render(
-  <BrowserRouter>
+  <Router>
     <App />
-  </BrowserRouter>,
+  </Router>,
   document.getElementById('root') // Monta la aplicación en el elemento con id 'root'
 );
